b4db14e469f77ce4f27d9bd6d428f387bd1da365
YangMr <759168442@qq.com>
2019-10-10 10:49:48 +0800

    node 12

--- 10day/yangmr-1904A-20191008/routes/index.js
+++ 10day/yangmr-1904A-20191008/routes/index.js
@@ -7,30 +7,43 @@ var fs = require("fs");
 
 router.post("/dologin",function (req,res,next) {
 
-    var obj = JSON.parse(fs.readFileSync("./data/data.json","utf8"))
+    var obj =fs.readFileSync("./data/data.json","utf8");
+    var user = JSON.parse(obj).users;
+
 
     var data = req.body;
 
-    for(var i=0;i<obj.users.length;i++){
-        if(obj.users[i].name == data.name && obj.users[i].pass == data.pass){
 
-            res.redirect("/success")
+    for(var i=0;i<user.length;i++){
 
-            // res.send({
-            //     code : 0,
-            //     message : "登录成功"
-            // })
-        }else{
-
-            res.redirect("/login")
-
-
-            // res.send({
-            //     code : 1,
-            //     message : "登录失败"
-            // })
-        }
+       if(user[i].name == data.name && user[i].pass==data.pass){
+           res.redirect("/success")
+       }else{
+           res.redirect("/login")
+       }
     }
+
+    // for(var i=0;i<obj.length;i++){
+    //     console.log(obj.name)
+    //     if(obj.users[i].name == data.name && obj.users[i].pass == data.pass){
+    //
+    //         res.redirect("/success")
+    //
+    //         // res.send({
+    //         //     code : 0,
+    //         //     message : "登录成功"
+    //         // })
+    //     }else{
+    //
+    //         res.redirect("/login")
+    //
+    //
+    //         // res.send({
+    //         //     code : 1,
+    //         //     message : "登录失败"
+    //         // })
+    //     }
+    // }
 })
 
 router.get("/login",function (req,res,next) {
